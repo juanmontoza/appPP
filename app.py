f7ba8b19db944d0b3787ae49d95c57bc6a9fa2ef
juanmontoza <juan.montoza@icloud.com>
2023-07-04 19:43:40 +0200

    adding hover tool

--- app.py
+++ app.py
@@ -34,6 +34,11 @@ if file is not None:
         # Let the user select the column for the y axis
         y_col = st.sidebar.selectbox('Select the column for the y axis', df.columns, key='y_col')
 
+        # Calculate the derivative for x_col
+        derivative_values = (df[original_col_x].shift(-interval_x) - df[original_col_x]) / (df[y_col].shift(-interval_x) - df[y_col])
+        df.loc[1:interval_x, 'derivative'] = np.nan
+        df.loc[interval_x + 1:, 'derivative'] = derivative_values
+
     else:
         # Let the user select the column for the y axis
         y_col = st.sidebar.selectbox('Select the column for the y axis', df.columns, key='y_col')
@@ -48,28 +53,23 @@ if file is not None:
         # Let the user select the column for the x axis
         x_col = st.sidebar.selectbox('Select the column for the x axis', df.columns, key='x_col_y')
 
-    # Check if the user selected "derivative" for x_col or y_col
-    if 'derivative' in [x_col, y_col]:
-        if x_col == 'derivative':
-            # Calculate the derivative for x_col
-            derivative_values = (df[original_col_x].shift(-interval_x) - df[original_col_x]) / (df[y_col].shift(-interval_x) - df[y_col])
-            df.loc[1:interval_x, 'derivative'] = np.nan
-            df.loc[interval_x + 1:, 'derivative'] = derivative_values
-
-        if y_col == 'derivative':
-            # Calculate the derivative for y_col
-            derivative_values = (df[x_col].shift(-interval_y) - df[x_col]) / (df[original_col_y].shift(-interval_y) - df[original_col_y])
-            df.loc[1:interval_y, 'derivative'] = np.nan
-            df.loc[interval_y + 1:, 'derivative'] = derivative_values
+        # Calculate the derivative for y_col
+        derivative_values = (df[x_col].shift(-interval_y) - df[x_col]) / (df[original_col_y].shift(-interval_y) - df[original_col_y])
+        df.loc[1:interval_y, 'derivative'] = np.nan
+        df.loc[interval_y + 1:, 'derivative'] = derivative_values
 
     # Create the plot using Seaborn
+    fig, ax = plt.subplots()
     if 'derivative' not in [x_col, y_col]:
-        fig, ax = plt.subplots()
         sns.scatterplot(data=df, x=x_col, y=y_col, ax=ax)
-        points = ax.collections[0]
-        cursor = mplcursors.cursor(points)
-        cursor.connect(
-            "add",
-            lambda sel: sel.annotation.set_text(f"({sel.target[0]:.2f}, {sel.target[1]:.2f})")
-        )
-        st.pyplot(fig)
+    else:
+        sns.scatterplot(data=df, x=x_col, y=y_col, ax=ax, label='Original')
+        sns.scatterplot(data=df, x=x_col, y='derivative', ax=ax, label='Derivative')
+
+    points = ax.collections[0]
+    cursor = mplcursors.cursor(points)
+    cursor.connect(
+        "add",
+        lambda sel: sel.annotation.set_text(f"({sel.target[0]:.2f}, {sel.target[1]:.2f})")
+    )
+    st.pyplot(fig)
