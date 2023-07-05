853a8b818a4ba276782161419123a93739c74bb7
juanmontoza <juan.montoza@icloud.com>
2023-07-05 15:21:45 +0200

    additional plot

--- app.py
+++ app.py
@@ -10,58 +10,65 @@ st.title('Pluspetrol Template')
 # Add a sidebar
 st.sidebar.title('Options')
 
-# Upload the Excel file
-file = st.sidebar.file_uploader('Upload your Excel file', type=['xlsx', 'xls', 'xlsm'])
+# Checkbox to add additional plot
+add_additional_plot = st.checkbox('Add additional plot')
 
-if file is not None:
-    # Load the file into a pandas DataFrame
-    df = pd.read_excel(file)
+if add_additional_plot:
+    # Add the sidebar options for the additional plot
+    st.sidebar.header('Additional Plot Options')
 
-    # Create a "derivative" column
-    df['derivative'] = np.nan
+    # Upload the Excel file
+    file = st.sidebar.file_uploader('Upload your Excel file', type=['xlsx', 'xls', 'xlsm'])
 
-    # Let the user select the columns for the x and y axes
-    x_col = st.sidebar.selectbox('Select the column for the x axis', df.columns, key='x_col')
-    y_col = st.sidebar.selectbox('Select the column for the y axis', df.columns, key='y_col')
+    if file is not None:
+        # Load the file into a pandas DataFrame
+        df = pd.read_excel(file)
 
-    add_derivative = st.sidebar.checkbox('Calculate Derivative')
+        # Create a "derivative" column
+        df['derivative'] = np.nan
 
-    if add_derivative:
-        interval = st.sidebar.number_input('Enter the interval for derivative calculation', min_value=1, value=10)
+        # Let the user select the columns for the x and y axes
+        x_col = st.sidebar.selectbox('Select the column for the x axis', df.columns, key='x_col')
+        y_col = st.sidebar.selectbox('Select the column for the y axis', df.columns, key='y_col')
 
-        if x_col != 'derivative':
-            derivative_values = (df[y_col].shift(-interval) - df[y_col]) / (df[x_col].shift(-interval) - df[x_col])
-            df.loc[1:interval, 'derivative'] = np.nan
-            df.loc[interval + 1:, 'derivative'] = derivative_values
+        add_derivative = st.sidebar.checkbox('Calculate Derivative')
 
-        if y_col != 'derivative':
-            derivative_values = (df[x_col].shift(-interval) - df[x_col]) / (df[y_col].shift(-interval) - df[y_col])
-            df.loc[1:interval, 'derivative'] = np.nan
-            df.loc[interval + 1:, 'derivative'] = derivative_values
+        if add_derivative:
+            interval = st.sidebar.number_input('Enter the interval for derivative calculation', min_value=1, value=10)
 
-    # Set the axis ranges
-    x_range_min = st.sidebar.number_input('Set the minimum value for the x-axis', value=df[x_col].min())
-    x_range_max = st.sidebar.number_input('Set the maximum value for the x-axis', value=df[x_col].max())
-    y_range_min = st.sidebar.number_input('Set the minimum value for the y-axis', value=df[y_col].min())
-    y_range_max = st.sidebar.number_input('Set the maximum value for the y-axis', value=df[y_col].max())
+            if x_col != 'derivative':
+                derivative_values = (df[y_col].shift(-interval) - df[y_col]) / (df[x_col].shift(-interval) - df[x_col])
+                df.loc[1:interval, 'derivative'] = np.nan
+                df.loc[interval + 1:, 'derivative'] = derivative_values
 
-    # Create the plot using Matplotlib
-    fig, ax = plt.subplots()
+            if y_col != 'derivative':
+                derivative_values = (df[x_col].shift(-interval) - df[x_col]) / (df[y_col].shift(-interval) - df[y_col])
+                df.loc[1:interval, 'derivative'] = np.nan
+                df.loc[interval + 1:, 'derivative'] = derivative_values
 
-    if add_derivative:
-        derivative_col = 'derivative' if x_col != 'derivative' else y_col
-        ax.scatter(df[x_col], df[derivative_col], s=5, color='red', label='Derivative')
-        ax.set_xlabel(x_col)
-        ax.set_ylabel('Derivative of {} '.format(y_col if y_col != 'derivative' else x_col))
-    else:
-        ax.set_xlabel(x_col)
-        ax.set_ylabel(y_col)
-        ax.scatter(df[x_col], df[y_col], s=5, color='blue', label=y_col)
+        # Set the axis ranges
+        x_range_min = st.sidebar.number_input('Set the minimum value for the x-axis', value=df[x_col].min())
+        x_range_max = st.sidebar.number_input('Set the maximum value for the x-axis', value=df[x_col].max())
+        y_range_min = st.sidebar.number_input('Set the minimum value for the y-axis', value=df[y_col].min())
+        y_range_max = st.sidebar.number_input('Set the maximum value for the y-axis', value=df[y_col].max())
 
-    ax.set_xlim([x_range_min, x_range_max])
-    ax.set_ylim([y_range_min, y_range_max])
-    ax.legend()
-    st.pyplot(fig)
+        # Create the plot using Matplotlib
+        fig, ax = plt.subplots()
 
-    # Show the DataFrame with the calculated derivative
-    st.write(df)
+        if add_derivative:
+            derivative_col = 'derivative' if x_col != 'derivative' else y_col
+            ax.scatter(df[x_col], df[derivative_col], s=5, color='red', label='Derivative')
+            ax.set_xlabel(x_col)
+            ax.set_ylabel('Derivative of {} (degrees)'.format(y_col if y_col != 'derivative' else x_col))
+        else:
+            ax.set_xlabel(x_col)
+            ax.set_ylabel(y_col)
+            ax.scatter(df[x_col], df[y_col], s=5, color='blue', label=y_col)
+
+        ax.set_xlim([x_range_min, x_range_max])
+        ax.set_ylim([y_range_min, y_range_max])
+        ax.legend()
+        st.pyplot(fig)
+
+        # Show the DataFrame with the calculated derivative
+        st.write(df)
