42fb120f7111a06c6682c5db840365702bc46cc6
juanmontoza <juan.montoza@icloud.com>
2023-07-05 17:26:27 +0200

    adding angle of attack

--- app.py
+++ app.py
@@ -13,6 +13,26 @@ st.sidebar.title('Options')
 # Upload the Excel file
 file = st.sidebar.file_uploader('Upload your Excel file', type=['xlsx', 'xls', 'xlsm'])
 
+
+def calculate_intersection_angle(segment1, segment2):
+    x1, y1 = segment1[0]
+    x2, y2 = segment1[1]
+    x3, y3 = segment2[0]
+    x4, y4 = segment2[1]
+
+    slope1 = (y2 - y1) / (x2 - x1)
+    slope2 = (y4 - y3) / (x4 - x3)
+
+    if slope1 == slope2:
+        return None  # Lines are parallel, no intersection angle
+
+    angle1 = math.atan(slope1)
+    angle2 = math.atan(slope2)
+
+    intersection_angle = abs(angle1 - angle2)
+    return math.degrees(intersection_angle)
+
+
 if file is not None:
     # Load the file into a pandas DataFrame
     df = pd.read_excel(file)
@@ -20,9 +40,23 @@ if file is not None:
     # Create a "derivative" column
     df['derivative'] = np.nan
 
+    # Create an "Angle of Attack" column
+    df['Angle of Attack'] = np.nan
+
     # Let the user select the columns for the x and y axes of the first plot
     x_col = st.sidebar.selectbox('Select the column for the x axis of the first plot', df.columns, key='x_col')
     y_col = st.sidebar.selectbox('Select the column for the y axis of the first plot', df.columns, key='y_col')
+
+    if y_col == 'Angle of Attack':
+        segment1_length = st.sidebar.number_input('Enter the length of Segment 1', min_value=1, value=10)
+        segment2_length = st.sidebar.number_input('Enter the length of Segment 2', min_value=1, value=10)
+
+        for i in range(len(df) - segment2_length):
+            segment1 = [(df['UM'].iloc[i], df[y_col].iloc[i]), (df['UM'].iloc[i+segment1_length], df[y_col].iloc[i+segment1_length])]
+            segment2 = [(df['TVDa'].iloc[i], df[y_col].iloc[i]), (df['TVDa'].iloc[i+segment2_length], df[y_col].iloc[i+segment2_length])]
+            angle = calculate_intersection_angle(segment1, segment2)
+            if angle is not None:
+                df.at[i, 'Angle of Attack'] = angle
 
     add_derivative = st.sidebar.checkbox('Calculate Derivative')
 
@@ -56,7 +90,11 @@ if file is not None:
         derivative_col = 'derivative' if x_col != 'derivative' else y_col
         ax.scatter(df[x_col], df[derivative_col], s=5, color='red', label='Derivative')
         ax.set_xlabel(x_col)
-        ax.set_ylabel('Derivative of {} (degrees)'.format(y_col if y_col != 'derivative' else x_col))
+        ax.set_ylabel('Derivative of {} '.format(y_col if y_col != 'derivative' else x_col))
+    elif y_col == 'Angle of Attack':
+        ax.set_xlabel('UM')
+        ax.set_ylabel('Angle of Attack (degrees)')
+        ax.scatter(df['UM'], df['Angle of Attack'], s=5, color='green', label='Angle of Attack')
     else:
         ax.set_xlabel(x_col)
         ax.set_ylabel(y_col)
